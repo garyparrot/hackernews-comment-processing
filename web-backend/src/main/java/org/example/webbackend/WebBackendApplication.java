153169dc26021a46ed9ee3cda35325655d378bfa
garyparrot <39105714+garyparrot@users.noreply.github.com>
2021-05-02 12:21:31 +0800

    Initialize web-backend

---
+++ web-backend/src/main/java/org/example/webbackend/WebBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.webbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBackendApplication.class, args);
+    }
+
+}
---
+++ web-backend/src/test/java/org/example/webbackend/WebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
