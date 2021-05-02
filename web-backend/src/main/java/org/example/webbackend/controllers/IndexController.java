87ac7756a438f95278a267bc1f88bd7c3ed7dd36
garyparrot <39105714+garyparrot@users.noreply.github.com>
2021-05-02 13:57:10 +0800

    initial

---
+++ web-backend/src/main/java/org/example/webbackend/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package org.example.webbackend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    private String index() {
+        return "home";
+    }
+}
