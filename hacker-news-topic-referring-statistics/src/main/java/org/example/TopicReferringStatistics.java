71bbb9c559618875318296790fb040fd6bdaf275
garyparrot <39105714+garyparrot@users.noreply.github.com>
2021-05-11 02:41:33 +0800

    fix: record deletion doesn't works for Postgres CDC Connector
    Delection of keyword record in Postgres will cause Postgres CDC
    Connector failed. This is due to My wrong understanding with Debezium Postgres
    Connector.
    
    This commit fix that. the code look bad because it work around many bad
    database schema design decision. I shouldn't insist to add a id record
    as a primary key to everything when there is a better option.

--- hacker-news-topic-referring-statistics/src/main/java/org/example/TopicReferringStatistics.java
+++ hacker-news-topic-referring-statistics/src/main/java/org/example/TopicReferringStatistics.java
@@ -22,9 +22,11 @@ public class TopicReferringStatistics {
     public static void main(String[] args) {
         final Serde<String> serdeString = Serdes.String();
         final Serde<Long> serdeLong = Serdes.Long();
+        final Serde<Integer> serdeInt = Serdes.Integer();
         final Serde<String[]> serdeStringArray = createJsonPOJOSerdes(String[].class);
         final Serde<CdcRecord> serdeCdcRecord = createJsonPOJOSerdes(CdcRecord.class);
         final Serde<GenericItem> serdeGenericItem = createJsonPOJOSerdes(GenericItem.class);
+        final Serde<KeywordAndCategories> serdeKAC = createJsonPOJOSerdes(KeywordAndCategories.class);
         final Serde<CategoriesAndOccurrence> serdeCategoriesAndOccurrence = createJsonPOJOSerdes(CategoriesAndOccurrence.class);
         final Serde<KeywordMatch> serdeKeywordMatch = new SpecificAvroSerde<>();
         final Map<String, String> serdeConfig = Collections.singletonMap("schema.registry.url", "http://localhost:8081");
@@ -39,8 +41,24 @@ public class TopicReferringStatistics {
 
         /* Declare keyword table */
         KTable<String, String[]> keywordTable = builder.stream("postgres-server.public.keywords", Consumed.with(serdeString, serdeCdcRecord))
+                // For some reason Postgres CDC Connector only know the primary key of the deleted record
+                // instead of all fields when Delete event occurred.
+                // We want the table to be KTable<Keyword, Categories>, but due to the above reason we can't do that.
+                // Now we need to make it KTable<Integer, (Keyword, Categories)>, so the delete function will work properly.
+                // After that we transform it back to what we want.
                 .flatMap(TopicReferringStatistics::flatMapPostgresCDCRecord)
-                .toTable(Materialized.with(serdeString, serdeStringArray));
+                .toTable(Materialized.with(serdeInt, serdeKAC))
+                // The following code will ...
+                // Transform KTable<Integer, (Keyword, Categories)>
+                //      into KTable<Keyword, Categories>
+                // The reason that we need to make things so complicated is probably due to the poorly designed database table :3
+                // If we use the Keyword string as the primary key instead of a meaningless id number then we won't have to suffer right now.
+                .groupBy((Integer integer, KeywordAndCategories kac) -> {
+                    return new KeyValue<>(kac.getKeyword(), kac.getCategories());
+                }, Grouped.with(serdeString, serdeStringArray)).reduce(
+                        (aggValue, newValue) -> newValue,
+                        (aggValue, oldValue) -> null
+                );
 
         /* Declare comment source */
         KStream<String, CategoriesAndOccurrence> wordMatches = builder.stream("hacker-news-comment", Consumed.with(serdeString, serdeGenericItem))
@@ -86,7 +104,7 @@ public class TopicReferringStatistics {
         System.exit(0);
     }
 
-    public static Iterable<KeyValue<? extends String, ? extends String[]>> flatMapPostgresCDCRecord(String s, CdcRecord cdcRecord) {
+    public static List<KeyValue<Integer, KeywordAndCategories>> flatMapPostgresCDCRecord(String s, CdcRecord cdcRecord) {
         if(cdcRecord == null){
             log.warn("Find a null CDC record with key: " +s);
             return Collections.emptyList();
@@ -98,22 +116,17 @@ public class TopicReferringStatistics {
             case Create:
             case Read:
                 assert after != null;
-                return Collections.singletonList(new KeyValue<>(after.getKeyword(), after.getCategory()));
+                return Collections.singletonList(new KeyValue<>(after.getId(), new KeywordAndCategories(after.getKeyword(), after.getCategory())));
             case Update:
                 assert before != null;
                 assert after != null;
-                List<KeyValue<? extends String, ? extends String[]>> updates = new ArrayList<>();
-                if(!before.getKeyword().equals(after.getKeyword())) {
-                    updates.add(new KeyValue<>(before.getKeyword(), null));
-                    updates.add(new KeyValue<>(after.getKeyword(), after.getCategory()));
-                } else if(!Arrays.equals(before.getCategory(), after.getCategory())) {
-                    assert before.getKeyword().equals(after.getKeyword());
-                    updates.add(new KeyValue<>(after.getKeyword(), after.getCategory()));
-                }
+                assert before.getId() == after.getId();
+                List<KeyValue<Integer, KeywordAndCategories>> updates = new ArrayList<>();
+                updates.add(new KeyValue<>(after.getId(), new KeywordAndCategories(after.getKeyword(), after.getCategory())));
                 return updates;
             case Delete:
                 assert before != null;
-                return Collections.singletonList(new KeyValue<>(before.getKeyword(), null));
+                return Collections.singletonList(new KeyValue<>(before.getId(), null));
         }
         throw new AssertionError();
     }
